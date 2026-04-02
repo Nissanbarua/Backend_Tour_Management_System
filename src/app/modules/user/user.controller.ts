99512721c8eeb2309521850c2566811eccdb83d1
Nissan Barua <baruanissan55@gmail.com>
2026-04-02 13:31:02 +0600

    catchAsync HOF and getAllUser created

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,22 +1,32 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status-codes";
 import { UserServices } from "./user.service";
+import { catchAsync } from "../../utils/catchAsync";
 
-const createUser = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    
+const createUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     const user = await UserServices.createUser(req.body);
     res.status(httpStatus.CREATED).json({
-      message: "User created succesfully",
+      success: true,
+      message: "Create user successfully",
       user,
     });
-  } catch (error: any) {
-    // console.log(error);
-    next(error);
-  }
-};
+  },
+);
+const getAllUsers = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const users = await UserServices.getAllUsers();
+
+    res.status(httpStatus.OK).json({
+      success: true,
+      message: "All user get successfully",
+      users,
+    });
+  },
+);
 
 export const UserController = {
   createUser,
+  getAllUsers,
 };
