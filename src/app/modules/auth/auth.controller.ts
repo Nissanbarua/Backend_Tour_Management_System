b5fc4b54869a5b17396effd036bea12d12d4cf31
Nissan Barua <baruanissan55@gmail.com>
2026-04-22 00:54:05 +0600

    set access and refresh Token in cookies

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -4,10 +4,21 @@ import { sendResponse } from "../../utils/sendResponse";
 import httpStatus from "http-status-codes";
 import { AuthServices } from "./auth.service";
 import { catchAsync } from "../../utils/catchAsync";
+import AppError from "../../errorHelpers/AppError";
 
 const credentialLogin = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const loginInfo = await AuthServices.credentialLogin(req.body);
+
+    res.cookie("accessToken", loginInfo.accessToken, {
+      httpOnly: true,
+      secure: false,
+    });
+
+    res.cookie("refreshToken", loginInfo.refreshToken, {
+      httpOnly: true,
+      secure: false,
+    });
 
     sendResponse(res, {
       success: true,
@@ -19,7 +30,13 @@ const credentialLogin = catchAsync(
 );
 const getNewAccessToken = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    const refreshToken = req.headers.authorization;
+    const refreshToken = req.cookies.refreshToken;
+    if (!refreshToken) {
+      throw new AppError(
+        httpStatus.BAD_GATEWAY,
+        "No refresh token found in cookie",
+      );
+    }
     const tokenInfo = await AuthServices.getNewAccessToken(
       refreshToken as string,
     );
